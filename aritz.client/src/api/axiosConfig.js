3e8b6f53d8ee1e8f92d0aae18e9d4fed59037bc6
Ramiro Unrein <unreinramiro2000@gmail.com>
2026-03-03 00:24:00 -0300

    Cambio en axiosConfig

--- aritz.client/src/api/axiosConfig.js
+++ aritz.client/src/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Crea una instancia de Axios con una configuración base
 const axiosInstance = axios.create({
-    baseURL: 'https://localhost:7273/api/'// Ruta base de tu backend
+    baseURL: import.meta.env.VITE_API_URL || 'https://localhost:7273/api/'// Ruta base de tu backend
 });
 
 export default axiosInstance;
